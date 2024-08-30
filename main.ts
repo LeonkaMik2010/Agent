68de3e01aa11c574da42183375e7c38ff4e62a46
LeonkaMik2010 <lemik2@outlook.cz>
2024-08-30 15:05:26 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,7 @@ player.onChat("ainfo", function () {
     player.say("/atp -> teleportne agenta na tvoji pozici natočeného na sever (Z-)")
     player.say("/avlevo (num1) -> otočí agenta směrem doleva (num1)x")
     player.say("/avpravo (num1) -> otočí agenta směrem doprava (num1)x")
-})
-player.onChat("astav", function (num1, num2, num3) {
-    agent.place(FORWARD)
+    player.say("/akopej tunel vpred")
 })
 player.onChat("avlevo", function (num1) {
     for (let index = 0; index < num1; index++) {
@@ -29,5 +27,24 @@ player.onChat("atp", function () {
 player.onChat("avpravo", function (num1) {
     for (let index = 0; index < num1; index++) {
         agent.turn(RIGHT_TURN)
+    }
+})
+player.onChat("akopej vpred", function (num1, num2, num3) {
+    for (let index = 0; index < num3; index++) {
+        agent.destroy(FORWARD)
+        agent.move(FORWARD, 1)
+        for (let index = 0; index < num1; index++) {
+            agent.destroy(UP)
+        }
+        if (num2 >= 2) {
+            agent.destroy(RIGHT)
+            for (let index = 0; index < num2 - 1; index++) {
+                agent.move(RIGHT, 1)
+                for (let index = 0; index < num1; index++) {
+                    agent.destroy(UP)
+                }
+            }
+            agent.move(LEFT, num2 - 1)
+        }
     }
 })
